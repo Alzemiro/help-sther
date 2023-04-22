acbe1ff43c788be6d140fc49860282d8b046730a
Alzemiro Iago de Moraes Thomaz <alzemiro.thomaz@supero.com.br>
2023-04-22 13:39:50 -0300

    Help Sther

--- main.js
+++ main.js
@@ -17,20 +17,20 @@ let countBar2 = 0;
 let interval1 = setInterval(() => {
     h1.textContent += nome1[countLetras1];
     countLetras1++;
-    if (countLetras1 == nome1.length){
+    if (countLetras1 == nome1.length) {
         bar1.style.display = "none";
         clearInterval(interval1);
         segundoTexto();
-    
+
     }
 }, timeLetras1);
 
-function segundoTexto(){
+function segundoTexto() {
     bar2.style.opacity = "1";
     let interval2 = setInterval(() => {
         h2.textContent += nome2[countLetras2];
         countLetras2++;
-        if (countLetras2 == nome2.length){
+        if (countLetras2 == nome2.length) {
             clearInterval(interval2);
             setInterval(() => {
                 if (countBar2 % 2 == 0) {
@@ -40,33 +40,34 @@ function segundoTexto(){
                 }
                 countBar2++;
             }, timeBar);
-            
+
         }
-           
+
     }, timeLetras1);
 }
 
 document.addEventListener("scroll", function() {
-    var pageTop = window.pageYOffset || document.documentElement.scrollTop;
-    var pageBottom = pageTop + window.innerHeight;
-    var tags = document.querySelectorAll(".tag");
-  
-   
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i];
-       
-      if (tag.getBoundingClientRect().top < pageBottom) {
-        tag.classList.add("visible");
-      } else {
-        tag.classList.remove("visible");
-      }
+    var ids = ['about', 'projects']
+    var elements = new Array();
+
+    for (const id of ids) {
+        elements.push(document.getElementById(id));
     }
 
-    if(scrollY < 100)   {
-        tags[0].classList.remove("visible")
-        
+    for (let element of elements) {
+        const rect = element.getBoundingClientRect();
+
+        // Obtém a altura da janela
+        const windowHeight = window.innerHeight;
+
+        // Calcula a posição vertical central da janela
+        const windowCenter = windowHeight / 2;
+
+        if (rect.top <= windowCenter && rect.bottom >= windowCenter) {
+            element.classList.add("visible");
+        } else {
+            element.classList.remove("visible");
+        }
     }
-  });
-  
 
-
+});
